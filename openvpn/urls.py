39df4f7442ee40e7727be5eca75e68089bc96129
kblockd <zhaozexing0922@gmail.com>
2017-04-18 11:27:21 +0800

    修改view逻辑

--- openvpn/urls.py
+++ openvpn/urls.py
@@ -3,6 +3,10 @@ from openvpn import views
 
 urlpatterns = [
 	url(r'^manage/status$',views.status_view,name='status'),
-    url(r'^manage',views.manage_view,name='manage'),
-
+	url(r'^manage/adduser$', views.adduser, name='adduser'),
+	url(r'^manage/addguser$', views.manage_view, name='addguser'),
+	url(r'^manage/deluser$', views.manage_view, name='deluser'),
+	url(r'^manage/delguser$', views.manage_view, name='delguser'),
+	url(r'^manage/delgroup$', views.manage_view, name='delgroup'),
+	url(r'^manage',views.manage_view,name='manage'),
 ]
--- openvpn/views.py
+++ openvpn/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic.detail import DetailView
+
 # Create your views here.
 
 def status_view(request):
@@ -11,21 +13,22 @@ def status_view(request):
 	return render(request, 'openvpn/status.html', {'status':p,'rows':p.count()}, content_type='text/html')
 
 def manage_view(request):
-	from openvpn.control import adduser
-	if request.method == 'POST':
-		if request.POST['action'] == 'adduser':
-			return render(request, 'openvpn/adduser.html', {'action':request.POST['action'].encode('utf-8')}, content_type='text/html')
-		elif request.POST['action'] == 'addgroup':
-			return render(request, 'openvpn/addgroup.html', {'action':request.POST['action'].encode('utf-8')}, content_type='text/html')
-		elif request.POST['action'] == 'addguser':
-			return render(request, 'openvpn/addguser.html', {'action': request.POST['action'].encode('utf-8')}, content_type='text/html')
-		elif request.POST['action'] == 'deluser':
-			return render(request, 'openvpn/deluser.html', {'action': request.POST['action'].encode('utf-8')}, content_type='text/html')
-		elif request.POST['action'] == 'delguser':
-			return render(request, 'openvpn/delguser.html', {'action': request.POST['action'].encode('utf-8')}, content_type='text/html')
-		elif request.POST['action'] == 'delgroup':
-			return render(request, 'openvpn/delgroup.html', {'action': request.POST['action'].encode('utf-8')}, content_type='text/html')
-		else:
-			return render(request, 'openvpn/manage.html', {}, content_type='text/html')
-	else:
-		return render(request, 'openvpn/manage.html', {}, content_type='text/html')
+	return render(request, 'openvpn/manage.html', {}, content_type='text/html')
+
+def adduser(request):
+	return render(request, 'openvpn/action.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
+
+def addgroup(request):
+	return render(request, 'openvpn/action.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
+
+def addguser(request):
+	return render(request, 'openvpn/action.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
+
+def deluser(request):
+	return render(request, 'openvpn/action.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
+
+def delguser(request):
+	return render(request, 'openvpn/action.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
+
+def delgroup(request):
+	return render(request, 'openvpn/actioin.html', {'action': request.POST['action'].encode('utf-8')},content_type='text/html')
